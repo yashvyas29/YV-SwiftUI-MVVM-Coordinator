e9f1b9408b98099b916bdf46428edbc156160128
Yash Vyas <vyasyas@hilti.com>
2022-03-12 16:04:49 +0530

    Initial Commit

---
+++ YV-SwiftUI-MVVM+Coordinator/YV_SwiftUI_MVVM_CoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  YV_SwiftUI_MVVM_CoordinatorApp.swift
+//  YV-SwiftUI-MVVM+Coordinator
+//
+//  Created by Yash Vyas on 12/03/22.
+//
+
+import SwiftUI
+
+@main
+struct YV_SwiftUI_MVVM_CoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
